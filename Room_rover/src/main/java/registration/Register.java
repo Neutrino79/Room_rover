c85ce35efedac45a04e0c0c349e50b8a95ea91ce
Neutrino79 <atharvhiremath.ah@gmail.com>
2023-11-01 00:00:23 +0530

    Added functionality to Registration poage
    -Added the basic functionality  -added form validation  -just need to solve how to display the error msg from servlet to the front end when the email gets repeeated

--- Room_rover/src/main/java/registration/Register.java
+++ Room_rover/src/main/java/registration/Register.java
@@ -9,8 +9,14 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import com.ConnInit.*;
+import java.util.Calendar;
+import java.util.Date;
 
 public class Register extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -23,7 +29,7 @@ public class Register extends HttpServlet {
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{
-		
+		String errorMessage = null;
 		Connection conn = null;
         PrintWriter out = response.getWriter();
         try 
@@ -31,9 +37,103 @@ public class Register extends HttpServlet {
         	 conn= DBConn.getConnection();
             if (conn != null) 
             {
-            	
                 out.println("Database Connection Successful");
-            } 
+                String name = request.getParameter("name");
+                String email = request.getParameter("email");
+                String pass = request.getParameter("password");
+                String rspass = request.getParameter("re_pass");
+                String connum = request.getParameter("contact");
+                String userType = request.getParameter("user_type");
+                String birthdt=request.getParameter("dob");
+                SimpleDateFormat inputDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+                SimpleDateFormat outputDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                
+                try 
+                {
+                	int rowsInserted=0;
+                	if("student".equals(userType))
+                	{
+                		String checkQuery = "SELECT * FROM Students WHERE email = ?";
+                        PreparedStatement checkPs = conn.prepareStatement(checkQuery);
+                        checkPs.setString(1, email);
+                        ResultSet checkRs = checkPs.executeQuery();
+
+                        if (checkRs.next()) 
+                        {
+                        	out.print("Email already exists. Please try a different one.");
+                        }
+                        else
+                        {
+                		    PreparedStatement ps = conn.prepareStatement("INSERT INTO Students(student_name, email, address, password, phone, aadhar, dob, gender, college_id) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)");
+                            ps.setString(1, name);
+                            ps.setString(2, email);
+                            ps.setString(3, "None"); // Address
+                            ps.setString(4, pass);
+                            ps.setString(5, connum);
+                            ps.setString(6, "None"); // Aadhar
+ 
+                            // Convert the date format and set it as a DATE
+                            java.util.Date parsedDate = inputDateFormat.parse(birthdt);
+                            java.sql.Date sqlDate = new java.sql.Date(parsedDate.getTime());
+                            ps.setDate(7, sqlDate);
+
+                            ps.setString(8, "None"); // Gender
+                            ps.setString(9, "None"); // College ID
+                            rowsInserted = ps.executeUpdate();
+                	    }
+                	}
+                	
+                	
+                	
+                	else if("owner".equals(userType))
+                	{
+                		    String checkQuery = "SELECT * FROM HostelOwners  WHERE email = ?";
+                            PreparedStatement checkPs = conn.prepareStatement(checkQuery);
+                            checkPs.setString(1, email);
+                            ResultSet checkRs = checkPs.executeQuery();
+
+                        if (checkRs.next()) 
+                        {
+                        	out.print("Email already exists. Please try a different one.");
+                        }
+                        else
+                        {
+                		    PreparedStatement ps = conn.prepareStatement("INSERT INTO HostelOwners(owner_name, email, password, contact, dob, owner_address) VALUES(?, ?, ?, ?, ?, ?)");
+                            ps.setString(1, name);
+                            ps.setString(2, email);
+                            ps.setString(3, pass);
+                            ps.setString(4, connum);
+                            // Convert the date format and set it as a DATE
+                            java.util.Date parsedDate = inputDateFormat.parse(birthdt);
+                            java.sql.Date sqlDate = new java.sql.Date(parsedDate.getTime());
+                            ps.setDate(5, sqlDate);
+                            ps.setString(6, "None"); // Owner address
+                            rowsInserted = ps.executeUpdate();
+                        }
+                	}
+
+                    if (rowsInserted > 0) 
+                    {
+                        // Registration is successful, redirect to the login page with a success message
+                       // response.sendRedirect("login.jsp?registrationSuccess=1");
+                    	out.println("Registratrion successfull");
+                    } 
+                    else 
+                    {
+                        // Registration failed, set an error message
+                        out.println("Registration failed. Please try again.");
+                    }
+                    
+                  
+                
+                }
+                catch (SQLException | ParseException e) 
+                {
+                    e.printStackTrace();
+                    out.println("Registration failed due to a database error: " + e.getMessage());
+                } 
+            }
             else 
             {
                 out.println("Database Connection Failed");
@@ -44,6 +144,7 @@ public class Register extends HttpServlet {
             e.printStackTrace();
             out.println("An error occurred: " + e.getMessage());
         } 
+        
         finally 
         {
             if (conn != null) 
@@ -52,60 +153,11 @@ public class Register extends HttpServlet {
                 {
                     conn.close();
                 } 
-                catch (SQLException e)
+                catch (SQLException e) 
                 {
                     e.printStackTrace();
                 }
             }
         }
-    
-
-        /*
-         * String sname = request.getParameter("name");
-        String semail = request.getParameter("email");
-        String spass = request.getParameter("pass");
-        String rspass = request.getParameter("re_pass");
-        String sconnum = request.getParameter("contact");
-       
-
-        try 
-        {
-            PreparedStatement ps = conn.prepareStatement("INSERT INTO stud(sname, semail, spass, scontact) VALUES(?,?,?,?)");
-            ps.setString(1, sname);
-            ps.setString(2, semail);
-            ps.setString(3, spass);
-            ps.setString(4, sconnum);
-
-            int rowsInserted = ps.executeUpdate();
-
-            if (rowsInserted > 0) 
-            {
-                // Registration is successful, redirect to the login page with a success message
-                response.sendRedirect("login.jsp?registrationSuccess=1");
-            } 
-            else 
-            {
-                // Registration failed, set an error message
-                out.println("Registration failed. Please try again.");
-            }
-        }
-        catch (SQLException e)
-        {
-            e.printStackTrace();
-            out.println("Registration failed due to a database error. Please try again later.");
-        } 
-        finally 
-        {
-            try 
-            {
-                conn.close();
-            } 
-            catch (SQLException e) 
-            {
-                e.printStackTrace();
-            }
-        }
-        */
 	}
-
 }
