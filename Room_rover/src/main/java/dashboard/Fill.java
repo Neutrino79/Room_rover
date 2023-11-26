40c12f6bc626ab30d143ee59a3be21fe7913b64c
Neutrino79 <atharvhiremath.ah@gmail.com>
2023-11-26 18:46:32 +0530

    temp commit before changes

--- Room_rover/src/main/java/dashboard/Fill.java
+++ Room_rover/src/main/java/dashboard/Fill.java
@@ -1,4 +1,5 @@
 package dashboard;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -14,6 +15,7 @@ import java.sql.SQLException;
 
 import com.ConnInit.DBConn;
 
+
 @WebServlet("/Prefill")
 public class Fill extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -23,107 +25,39 @@ public class Fill extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-    // Define HostelData as a static inner class
-    public static class HostelData {
-        private String hostelName;
-        private String location;
-        private int totalRooms;
-        private String roomType;
-        private int occupied;
-        private double price;
-        private String amenitiesString;
-        private String documentPath;
-
-        // Constructors
-        public HostelData() {
-        }
-
-        public HostelData(String hostelName, String location, int totalRooms, String roomType, int occupied,
-                          double price, String amenitiesString, String documentPath) {
-            this.hostelName = hostelName;
-            this.location = location;
-            this.totalRooms = totalRooms;
-            this.roomType = roomType;
-            this.occupied = occupied;
-            this.price = price;
-            this.amenitiesString = amenitiesString;
-            this.documentPath = documentPath;
-        }
-
-        // Getters and setters
-        public String getHostelName() {
-            return hostelName;
-        }
-
-        public void setHostelName(String hostelName) {
-            this.hostelName = hostelName;
-        }
-
-        public String getLocation() {
-            return location;
-        }
-
-        public void setLocation(String location) {
-            this.location = location;
-        }
-
-        public int getTotalRooms() {
-            return totalRooms;
-        }
-
-        public void setTotalRooms(int totalRooms) {
-            this.totalRooms = totalRooms;
-        }
-
-        public String getRoomType() {
-            return roomType;
-        }
-
-        public void setRoomType(String roomType) {
-            this.roomType = roomType;
-        }
-
-        public int getOccupied() {
-            return occupied;
-        }
-
-        public void setOccupied(int occupied) {
-            this.occupied = occupied;
-        }
-
-        public double getPrice() {
-            return price;
-        }
-
-        public void setPrice(double price) {
-            this.price = price;
-        }
-
-        public String getAmenitiesString() {
-            return amenitiesString;
-        }
-
-        public void setAmenitiesString(String amenitiesString) {
-            this.amenitiesString = amenitiesString;
-        }
-
-        public String getDocumentPath() {
-            return documentPath;
-        }
-
-        public void setDocumentPath(String documentPath) {
-            this.documentPath = documentPath;
-        }
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	System.out.println("i am in the doget servlt");
     }
+    @Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		//doPost(request, response);
+    	//doGet(request, response);
+		//response.setContentType("application/json");
+		
 		System.out.println("i am in the fill servlt");
-		response.setContentType("application/json");
-        PrintWriter out = response.getWriter();
+		PrintWriter out = response.getWriter();
+        String hostelIdParam = request.getParameter("hostelId");
 
-        int hostelId = Integer.parseInt(request.getParameter("hostelIdToPrefill"));
-
+        if (hostelIdParam != null && !hostelIdParam.isEmpty()) {
+            try {
+                int hostelId = Integer.parseInt(hostelIdParam);
+                System.out.println("Hostel id = " + hostelId);
+            } catch (NumberFormatException e) {
+                // Handle the case where "hostelId" is not a valid integer
+                System.err.println("Invalid hostelId format: " + hostelIdParam);
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                return;
+            }
+        } else {
+            // Handle the case where "hostelId" is null or empty.
+            // You might want to set a default value or return an error response.
+            System.err.println("Hostel id is missing or empty");
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+        
+        /*
         Connection conn = null;
         try {
             conn = DBConn.getConnection();
@@ -147,10 +81,10 @@ public class Fill extends HttpServlet {
 
                 HostelData hostelData = new HostelData(hostelName, location, totalRooms, roomType, occupied, price, amenitiesString, documentPath);
                 
-             //   ObjectMapper objectMapper = new ObjectMapper();
-             //   String jsonResponse = objectMapper.writeValueAsString(hostelData);
+                ObjectMapper objectMapper = new ObjectMapper();
+                String jsonResponse = objectMapper.writeValueAsString(hostelData);
 
-          //      out.print(jsonResponse);
+                out.print(jsonResponse);
             } else {
                 out.print("{\"error\": \"No data found for hostelId: " + hostelId + "\"}");
             }
@@ -167,7 +101,7 @@ public class Fill extends HttpServlet {
             } catch (SQLException e) {
                 e.printStackTrace();
             }
-        }
+        }*/
     }
 	
 
